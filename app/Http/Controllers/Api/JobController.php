ccd707c691f4f6edf7f01188fb1e89427e06acc3
mnurichsan <iccankblog@gmail.com>
2022-05-25 13:45:06 +0800

    added feature Filter Job

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseBuilder;
+use App\Models\Category;
 use App\Models\Job;
 
 class JobController extends Controller
@@ -12,7 +13,26 @@ class JobController extends Controller
     public function index(Request $request)
     {
         try {
-            $jobs = Job::get();
+            if($request->has('categoryId'))
+            {
+                $categoryId = $request->categoryId;
+                $jobs = Job::with('category','jobType','company')->whereHas('category', function ($query) use ($categoryId){
+                    $query->where('id',$categoryId);
+                })->get();
+            }
+            elseif($request->has('jobTypeId'))
+            {
+                $jobTypeId = $request->jobTypeId;
+                $jobs = Job::with('category','jobType','company')->whereHas('jobType', function ($query) use ($jobTypeId){
+                    $query->where('id',$jobTypeId);
+                })->get();
+
+            }
+            else{
+                $jobs = Job::with('category','jobType','company')->get();
+            }
+           
+
 
             return ResponseBuilder::success(message:'Jobs Fetch Successfully',data:$jobs,statusCode:201);
             
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = ['name','slug'];
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
