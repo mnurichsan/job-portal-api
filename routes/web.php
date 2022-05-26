3d3a751fc9b629c1482df1a99bd171f67ebe7d6c
mnurichsan <iccankblog@gmail.com>
2022-05-27 07:17:23 +0800

    add welcome response and fix done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\ResponseBuilder;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ResponseBuilder::success(message: 'Welcome To Job Portal API',statusCode: 200);
 });
